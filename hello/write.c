e636a643cbaae669d709b78ba719b70ac4a29cd8
Eric Smith <ersmith@totalspectrum.ca>
2019-04-03 18:35:11 -0300

    added directory hello, which uses newlib to run a program

---
+++ hello/write.c
@@ -0,0 +1,22 @@
+#include "../lib/riscv.h"
+#include <sys/stat.h>
+
+int
+_fstat(int fd, struct stat *buf)
+{
+    buf->st_mode = S_IFCHR;
+    buf->st_blksize = 0;
+    return 0;
+}
+
+int
+_write(int fd, unsigned char *data, int count)
+{
+    int c;
+    int orig_count = count;
+    while (count-- > 0) {
+        c = *data++;
+        csr_write(UART, c);
+    }
+    return orig_count;
+}

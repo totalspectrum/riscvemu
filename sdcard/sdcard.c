90ee9326d86cb304363003f2fd9fc26af74c768c
Eric Smith <ersmith@totalspectrum.ca>
2019-06-11 20:17:20 -0300

    begin work on sdcard support

---
+++ sdcard/sdcard.c
@@ -0,0 +1,225 @@
+/*
+ * SD Card test program
+ * designed for P2 version of RiscV emulator
+ * define USE_SMARTPIN to toggle using the smartpin NCO mode
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "../lib/riscv.h"
+#define drvl_(x) pinlow(x)
+#define drvh_(x) pinhigh(x)
+
+#define PIN_SS   60
+#define PIN_MOSI 59
+#define PIN_MISO 58
+#define PIN_CLK  61
+#define PIN_TEST (PIN_MOSI)
+
+extern void iprintf(const char *, ...);
+
+uint64_t getcycles()
+{
+    uint32_t hi, lo, h2;
+again:
+    h2 = getcnth();
+    lo = getcnt();
+    hi = getcnth();
+    if (hi != h2) goto again;
+    return ((uint64_t)hi) << 32 | lo;
+}
+
+#define spi_short_delay() waitcnt(getcnt() + 10)
+
+//
+// assume 160 MHz; then delay 160 -> 1 MHz, 1600 -> 10 KHz
+//
+static void delay_us(uint32_t us)
+{
+    us = 160 * us;
+    waitcnt(getcnt() + us);
+}
+
+static void spi_delay(void)
+{
+    delay_us(5);
+}
+
+static int spi_read()
+{
+    int i, r;
+    r = 0;
+    for (i = 0; i < 8; i++) {
+        drvl_(PIN_CLK);
+        drvh_(PIN_CLK);
+        spi_short_delay();
+        r = (r << 1) | getpin(PIN_MISO);
+    }
+    return r;
+}
+
+static int spi_chktimeout(uint32_t starttime) {
+    uint32_t now = getcnt();
+    if (now - starttime > 50000000) {
+        return 1;
+    }
+    return 0;
+}
+
+static int spi_readresp(void)
+{
+    int r;
+    uint32_t starttime = getcnt();
+    
+    while (1) {
+        r = spi_read();
+        if (r != 0xff) break;
+        if (spi_chktimeout(starttime)) {
+            return -1;
+        }
+    }
+    return r;
+}
+
+static void spi_send(int outv)
+{
+    int i;
+    for (i = 0; i < 8; i++) {
+        drvl_(PIN_CLK);
+        if (outv & 0x80) {
+            drvh_(PIN_MOSI);
+        } else {
+            drvl_(PIN_MOSI);
+        }
+        outv <<= 1;
+        drvh_(PIN_CLK);
+    }
+    drvh_(PIN_MOSI);
+}
+
+int spi_cmd(int index, int arg)
+{
+    drvl_(PIN_SS);
+    (void)spi_read();
+    spi_send(0x40+index);
+    spi_send((arg >> 24) & 0xff);
+    spi_send((arg >> 16) & 0xff);
+    spi_send((arg >> 8) & 0xff);
+    spi_send(arg & 0xff);
+
+    // CRC; this only matters for a few commands
+    if (index == 0) {
+        spi_send(0x95);
+    } else {
+        spi_send(0x87);
+    }
+    return spi_readresp();
+}
+
+void spi_endcmd(void)
+{
+    drvh_(PIN_SS);
+}
+
+bool sdcard_init(void)
+{
+    int i;
+    int x;
+    int retries;
+
+    delay_us(1000);
+    
+    dirl_(PIN_MISO);
+    dirh_(PIN_CLK);
+    dirh_(PIN_MOSI);
+    drvh_(PIN_SS);
+    drvh_(PIN_MOSI);
+    for (i = 0; i < 600; i++) {
+        (void) spi_read();
+    }
+
+    i = spi_cmd(0, 0);
+    spi_endcmd();
+    if (i < 0) {
+        iprintf("timeout 1 in init\n");
+        return false;
+    }
+    i = spi_cmd(8, 0x1aa);
+    spi_endcmd();
+    if (i < 0) {
+        iprintf("timeout 2 in init\n");
+        return false;
+    }
+    while (1) {
+        spi_cmd(55, 0);
+        i = spi_cmd(41, 0x40000000);
+        if (i == 0) break;
+        if (i < 0) {
+            iprintf("timeout in init loop\n");
+            return false;
+        }
+    }
+    i = spi_cmd(58, 0);
+    if (i >= 0) {
+        x = spi_read();
+    }
+    spi_endcmd();
+    if (i) {
+        iprintf("could not init card\n");
+        return false;
+    }
+    return true;
+}
+
+bool sdcard_is_present(void)
+{
+    int i = getpin(PIN_TEST);
+    return (i == 0);
+}
+
+uint64_t sdcard_get_capacity_in_bytes(void)
+{
+    return 0;
+}
+
+// these return 0 on success, non-zero on error
+uint32_t sdcard_read_blocks(uint8_t *dest, uint32_t block_num, uint32_t num_blocks)
+{
+    return -1;
+}
+uint32_t sdcard_write_blocks(uint8_t *dest, uint32_t block_num, uint32_t num_blocks)
+{
+    return -1;
+}
+
+void nap(uint64_t cycles)
+{
+    uint64_t now;
+    uint64_t end;
+    now = getcycles();
+    end = now + cycles;
+    do {
+        now = getcycles();
+    } while (now < end);
+}
+
+void main()
+{
+    bool ok;
+
+    nap(40000000);
+    for(;;) {
+        ok = sdcard_is_present();
+        iprintf("sdcard is %spresent\n", ok ? "" : "NOT ");
+        nap(160000000);
+        if (ok) {
+            break;
+        }
+    }
+    ok = sdcard_init();
+    if (ok) {
+        iprintf("sdcard OK\n");
+    } else {
+        iprintf("error\n");
+    }
+}

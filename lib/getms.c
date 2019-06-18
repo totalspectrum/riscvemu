6fb6ca715d6da1db454a30e4f179a967e7abc2ce
Eric Smith <ersmith@totalspectrum.ca>
2019-06-18 20:03:34 -0300

    use new millisecond counter for getms function

--- lib/getms.c
+++ lib/getms.c
@@ -1,7 +1,13 @@
-extern unsigned int getcnt();
+#include "riscv.h"
 
+#if 0
 extern unsigned int getcyclespersec();
 
 unsigned int getms() {
     return getcnt() / (getcyclespersec()/1000);
 }
+#else
+unsigned int getms() {
+    return getmillis() * 2;
+}
+#endif
